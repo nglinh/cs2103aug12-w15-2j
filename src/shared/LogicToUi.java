cac0c4082ecb24a40a73efb2b48ff9eaa07c9a0c
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm.comp.nus.edu.sg>
2012-10-12 11:47:00 +0800

    Task object now asserts description cannot be of 0 length

---
+++ src/shared/LogicToUi.java
@@ -0,0 +1,52 @@
+package shared;
+/**  
+ * LogicToUi.java 
+ * A class for to hold information returned from the Logic to the UI
+ * @author  Yeo Kheng Meng
+ */ 
+
+import java.util.ArrayList;
+
+
+public class LogicToUi {
+
+	String output = null;
+	ArrayList<Task> display = null;
+
+	
+	public LogicToUi(String output)	{
+		assert(output != null);
+		this.output = output;
+	}
+	
+	public LogicToUi(ArrayList<Task> display) {
+		assert(display != null);
+		this.display = display;
+	}
+	
+	
+	public boolean isReturnValueAString() {
+		if (this.output == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean isReturnValueAList() {
+		if (this.display == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	
+	public String getString() {
+		return output;
+	}
+	
+	public ArrayList<Task> getList() {
+		return display;
+	}
+}
