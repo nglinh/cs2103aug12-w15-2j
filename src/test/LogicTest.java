1da25a08a70f8cf7d730ae7c40c8f20dc24e289f
Kai Yao <Kai Yao@KaiYao-PC.lan>
2012-10-15 00:35:21 +0800

    Made LogicTest a bit easier to change response format in future. Added a few more test cases.

--- src/test/LogicTest.java
+++ src/test/LogicTest.java
@@ -15,26 +15,30 @@ public class LogicTest {
 		
 		// Add normal timed task with starting and ending time and date
 		command = "add My New Task from 25 Sep 2012 8pm to 26 Sep 2012 9pm";
-		expectedResponse = "Timed task \"My New Task\" from 25 Sep 2012 8:00pm to 26 Sep 2012 9:00pm added";
+		expectedResponse = timedTaskResponse("My New Task", "Tue 25 Sep 2012 8:00PM", "Wed 26 Sep 2012 9:00PM");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
 
 		// Add normal timed task with starting and ending date but no time.
 		// Assume 12:00am for start time and 23:59pm for end time.
 		command = "add My New Task from 25 Sep 2012 to 26 Sep 2012";
-		expectedResponse = "Timed task \"My New Task\" from 25 Sep 2012 12:00am to 26 Sep 2012 11:59pm added";
+		expectedResponse = timedTaskResponse("My New Task", "Tue 25 Sep 2012 12:00AM", "Wed 26 Sep 2012 11:59PM");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
 		
 		// Add normal timed task with starting and ending time and date
 		// but with "from" inside the text
 		command = "add Pick up groceries from supermarket from 25 Sep 2012 8pm to 26 Sep 2012 9pm";
-		expectedResponse = "Timed task \"Pick up groceries from supermarket\" from 25 Sep 2012 8:00pm to 26 Sep 2012 9:00pm added";
+		expectedResponse = timedTaskResponse("Pick up groceries from supermarket", "Tue 25 Sep 2012 8:00PM", "Wed 26 Sep 2012 9:00PM");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
 		
 		// Add normal timed task with starting and ending time and date
 		// but with "to" inside the text
 		command = "add Go to supermarket from 25 Sep 2012 8pm to 26 Sep 2012 9pm";
-		expectedResponse = "Timed task \"Go to supermarket\" from 25 Sep 2012 8:00pm to 26 Sep 2012 9:00pm added";
+		expectedResponse = timedTaskResponse("Go to supermarket", "Tue 25 Sep 2012 8:00PM", "Wed 26 Sep 2012 9:00PM");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
+	}
+	
+	public String timedTaskResponse(String taskName, String startDate, String endDate){
+		return "Timed task \""+taskName+"\" from "+startDate+" to "+endDate+" added";
 	}
 	
 	@Test
@@ -43,20 +47,36 @@ public class LogicTest {
 		
 		// Add normal deadline task with ending time and date
 		command = "add Do homework by 26 Sep 2012 9pm";
-		expectedResponse = "Deadline task \"Do homework\" by 26 Sep 2012 9:00pm added";
+		expectedResponse = deadlineTaskResponse("Do homework", "Wed 26 Sep 2012 9:00PM");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
 
 		// Add normal deadline task with ending date but no time.
 		// Assume 11:59pm for time.
 		command = "add Do homework by 26 Sep 2012";
-		expectedResponse = "Deadline task \"Do homework\" by 26 Sep 2012 11:59pm added";
+		expectedResponse = deadlineTaskResponse("Do homework", "Wed 26 Sep 2012 11:59PM");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
 		
 		// Add normal deadline task with starting and ending time and date
 		// but with "by" inside the text
 		command = "add Read book by Steven by 26 Sep 2012 9pm";
-		expectedResponse = "Deadline task \"Read book by Steven\" by 26 Sep 2012 9:00pm added";
+		expectedResponse = deadlineTaskResponse("Read book by Steven", "Wed 26 Sep 2012 9:00PM");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
+		
+		// Add deadline task with the word "at"
+		command = "add go to meeting at 15 Oct 2012 noon";
+		expectedResponse = deadlineTaskResponse("go to meeting", "Mon 15 Oct 2012 12:00PM");
+		
+		/* Commented out as this the interpretation can be correct in different ways
+		
+		// Add deadline task with only year
+		command = "add fly to Germany by 2013";
+		expectedResponse = deadlineTaskResponse("fly to Germany", "Tue 31 Dec 2013 23:59PM");
+		
+		*/
+	}
+	
+	public String deadlineTaskResponse(String taskName, String endDate){
+		return "Deadline task \""+taskName+"\" by "+endDate+" added";
 	}
 	
 	@Test
@@ -65,23 +85,32 @@ public class LogicTest {
 		
 		// Add normal floating task
 		command = "add Just a normal floating task";
-		expectedResponse = "Floating task \"Just a normal floating task\" added";
+		expectedResponse = floatingTaskResponse("Just a normal floating task");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
 
 		// Add floating task with tricky words "from"
 		command = "add Print photos from Alan";
-		expectedResponse = "Floating task \"Print photos from Alan\" added";
+		expectedResponse = floatingTaskResponse("Print photos from Alan");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
 		
 		// Add floating task with tricky words "by"
 		command = "add Print photos taken by Alan";
-		expectedResponse = "Floating task \"Print photos taken by Alan\" added";
+		expectedResponse = floatingTaskResponse("Print photos taken by Alan");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
 		
 		// Add floating task with tricky words "to"
 		command = "add Bring photos to printing shop";
-		expectedResponse = "Floating task \"Bring photos to printing shop\" added";
+		expectedResponse = floatingTaskResponse("Bring photos to printing shop");
 		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
+		
+		// Add floating task with tricky words "market" (starts with mar = march)
+		command = "add Go to market";
+		expectedResponse = floatingTaskResponse("Go to market");
+		assertEquals(Logic.uiCommunicator(command).getString(), expectedResponse);
+	}
+	
+	public String floatingTaskResponse(String taskName){
+		return "Floating task \""+taskName+"\" added";
 	}
 	
 	@Test
