14abbcea8b2d16c4b5d232ea3fb82aba2dac5c4d
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm>
2012-10-15 22:38:09 +0800

    Cli: Add extra column border line for tasks whose names overflow

---
+++ src/logic/AddParser.java
@@ -0,0 +1,129 @@
+package logic;
+
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.joda.time.DateTime;
+
+import shared.Task.TaskType;
+
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
+
+public class AddParser {
+	private List<DateGroup> groups;
+	private Parser parser;
+	private String argument;
+	private TaskType taskType;
+	private int matchingPosition;
+	public AddParser(String str){
+		argument = str;
+		String dateString = removeTriggerWord(argument);
+		matchingPosition = 0;
+		parser = new Parser(TimeZone.getDefault());
+		groups = parser.parse(dateString);
+		if(this.getTaskType()!= TaskType.FLOATING){
+			int location = groups.get(0).getPosition();
+			matchingPosition = location;
+			while(!checkFullWord(dateString,location)){
+				char[] tempCharArray = dateString.toCharArray();
+				String tempString = "";
+				int i = location;
+				while(i<tempCharArray.length && tempCharArray[i]!=' '){
+					i++;
+				}
+				if(i == tempCharArray.length){
+					dateString = argument.trim();
+					location = argument.length();
+					matchingPosition = location;
+					groups = parser.parse("");
+					break;
+				}
+				matchingPosition += i-location+1;
+				for(int k = i;k<dateString.length();k++){
+					tempString = tempString + tempCharArray[k];
+				}
+				dateString = tempString.trim();
+				groups = parser.parse(dateString);
+				location = groups.get(0).getPosition();
+				matchingPosition += location;
+			}
+		}
+	}
+	private String removeTriggerWord(String argument2) {
+		String result = "";
+		String[] tempStringArray = argument2.split(" ");
+		for (int i =0;i<tempStringArray.length;++i){
+			if(tempStringArray[i].compareTo("from")==0 )
+				result = result + "    ";
+			else if(tempStringArray[i].compareTo("by")==0)
+				result = result + "   ";
+			else if(tempStringArray[i].compareTo("before")==0)
+				result = result + "       ";
+			else if(tempStringArray[i].compareTo("on")==0)
+				result = result +"   ";
+			else
+				result = result+ tempStringArray[i]+" ";
+		}
+		return result.trim();
+	}
+	private boolean checkFullWord(String dateString, int location) {
+		char[] tempCharArray = dateString.toCharArray();
+		String matchingValue = groups.get(0).getText();
+		char[] matchingArray = matchingValue.toCharArray();
+		if(location+matchingArray.length < tempCharArray.length &&
+				tempCharArray[location+matchingArray.length]!= ' ')
+			return false;
+		return true;
+	}
+	public TaskType getTaskType(){
+		if(groups.size()==0)
+			taskType = TaskType.FLOATING;
+		else if(groups.get(0).getDates().size()==1){
+			taskType =  TaskType.DEADLINE;
+		}
+		else 
+			taskType = TaskType.TIMED;
+		return taskType;
+	}
+	public String getTaskDescription(){
+		if(taskType == TaskType.FLOATING)
+			return argument;
+		else{
+			return buildString(argument,this.matchingPosition);
+		}
+	}
+	private String buildString(String argument2, int matchingPosition2) {
+		String result = "";
+		char[] tempCharArray = argument2.toCharArray();
+		for(int i =0;i<matchingPosition2;++i)
+			result = result+tempCharArray[i];
+		result = result.trim();
+		String[] tempStringArray = result.split(" ");
+		if(tempStringArray[tempStringArray.length-1].compareTo("from")==0||
+				tempStringArray[tempStringArray.length-1].compareTo("by")==0||
+				tempStringArray[tempStringArray.length-1].compareTo("before")==0||
+				tempStringArray[tempStringArray.length-1].compareTo("on")==0){
+			tempStringArray[tempStringArray.length-1] = "";
+			result = "";
+			for(int i=0;i<tempStringArray.length;++i)
+				result = result + tempStringArray[i]+" ";
+			result = result.trim();
+		}
+		return result;
+	}
+	public DateTime getBeginTime(){
+		Date st = this.getGroups().get(0).getDates().get(0);
+		DateTime result = new DateTime(st);
+		return result;
+	}
+	public DateTime getEndTime(){
+		Date et = this.getGroups().get(0).getDates().get(1);
+		DateTime result = new DateTime(et);
+		return result;
+	}
+	public List<DateGroup> getGroups(){
+		return this.groups;
+	}
+}
---
+++ src/ui/GuiMain.java
@@ -0,0 +1,425 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.BorderLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenu;
+import javax.swing.JEditorPane;
+
+import shared.LogicToUi;
+import shared.Task;
+import shared.Task.TaskType;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+import javax.swing.JPanel;
+
+import javax.swing.event.HyperlinkListener;
+import javax.swing.event.HyperlinkEvent;
+
+public class GuiMain extends UI{
+
+	private static final String TABLE_EMPTY_DATE_FIELD = "";
+	private JFrame frmDoit;
+	private JTextField textCmd;
+	private JScrollPane scrollPane;
+	private JTable table;
+	private JPopupMenu popupCmdHint;
+	private JPopupMenu popupStatus;
+	private JMenu mnSomeMenu;
+	private JEditorPane txtCmdHint;
+	private JEditorPane txtStatus;
+	private JPanel panel;
+	
+	private List<String> commandHistory;
+	private ListIterator<String> commandHistoryIterator;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		new GuiMain().runUI();
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GuiMain() {
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	@SuppressWarnings("serial")
+	private void initialize() {
+		frmDoit = new JFrame();
+		frmDoit.setTitle("DoIt!");
+		frmDoit.setBounds(100, 100, 700, 400);
+		frmDoit.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JMenuBar menuBar = new JMenuBar();
+		frmDoit.setJMenuBar(menuBar);
+
+		mnSomeMenu = new JMenu("Some Menu");
+		mnSomeMenu.setMnemonic('s');
+		menuBar.add(mnSomeMenu);
+		
+		panel = new JPanel();
+		frmDoit.getContentPane().add(panel, BorderLayout.SOUTH);
+				panel.setLayout(new BorderLayout(0, 0));
+		
+		
+		
+				textCmd = new JTextField();
+				panel.add(textCmd);
+				
+				commandHistory = new LinkedList<String>();
+				commandHistoryIterator = commandHistory.listIterator();
+				
+				textCmd.addKeyListener(new KeyAdapter() {
+					@Override
+					public void keyReleased(KeyEvent arg0) {
+						if (arg0.getKeyCode() == java.awt.event.KeyEvent.VK_ENTER) {
+							commandHistory.add(textCmd.getText());
+							commandHistoryIterator = commandHistory.listIterator(commandHistory.size());
+							
+							executeCommand(textCmd.getText());
+							popupCmdHint.setVisible(false);
+						} else if (arg0.getKeyCode() == java.awt.event.KeyEvent.VK_UP) {
+							if (commandHistoryIterator.hasPrevious()) {
+								textCmd.setText(commandHistoryIterator.previous());
+								showHint();
+							}
+						} else if (arg0.getKeyCode() == java.awt.event.KeyEvent.VK_DOWN) {
+							if (commandHistoryIterator.hasNext()) {
+								textCmd.setText(commandHistoryIterator.next());
+								showHint();
+							}
+						} else {
+							showHint();
+						}
+					}
+
+					private void showHint() {
+						if (textCmd.getText().startsWith("add")) {
+							txtCmdHint.setText("<html>\r\n<font face=\"Tahoma, Arial, Sans-serif\">\r\n<font size=\"4\">\r\n<b>add</b><br>\r\nAdds a new task<br>\r\n<br>\r\nExamples:<br>\r\n</font>\r\n<font size=\"3\">\r\n<b>add</b> Meeting <b>from</b> 2pm 25/9 <b>to</b> 3pm 25/9<br>\r\n<b>add</b> Complete report <b>by</b> 5pm 25/9<br>\r\n<b>add</b> Search for document<br>\r\n</font>\r\n</font>\r\n</html>");
+							popupCmdHint.setPopupSize(200, 120);
+							popupCmdHint.show(textCmd, 5, textCmd.getHeight());
+							textCmd.requestFocus();
+						} else if (textCmd.getText().startsWith("list")) {
+							txtCmdHint.setText("<html>\r\n<font face=\"Tahoma, Arial, Sans-serif\">\r\n<font size=\"4\">\r\n<b>list</b><br>\r\nList tasks<br>\r\n<br>\r\nExamples:<br>\r\n</font>\r\n<font size=\"3\">\r\n<b>list</b><br><b>list all</b><br><b>list done</b><br><b>list etc.</b>\r\n</font>\r\n</font>\r\n</html>");
+							popupCmdHint.setPopupSize(200, 140);
+							popupCmdHint.show(textCmd, 5, textCmd.getHeight());
+							textCmd.requestFocus();
+						} else {
+							popupCmdHint.setVisible(false);
+						}
+					}
+				});
+		textCmd.setColumns(10);
+
+		popupCmdHint = new JPopupMenu();
+		addPopup(textCmd, popupCmdHint);
+
+		txtCmdHint = new JEditorPane();
+		txtCmdHint.setEditable(false);
+		txtCmdHint.setContentType("text/html");
+		txtCmdHint.setBackground(new Color(0, 0, 0, 0));
+		txtCmdHint.setOpaque(false);
+		popupCmdHint.add(txtCmdHint);
+
+		txtStatus = new JEditorPane();
+		txtStatus.addHyperlinkListener(new HyperlinkListener() {
+			public void hyperlinkUpdate(HyperlinkEvent event) {
+				if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+					if (event.getURL().toString().endsWith("/undo")) {
+						executeCommand("undo");
+					}
+				}
+			}
+		});
+		txtStatus.setBackground(new Color(0, 0, 0, 0));
+		txtStatus.setOpaque(false);
+		panel.add(txtStatus, BorderLayout.SOUTH);
+		txtStatus.setEditable(false);
+		txtStatus.setContentType("text/html");
+
+		scrollPane = new JScrollPane();
+		frmDoit.getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+		table = new JTable();
+		// Dummy table for WindowBuilder, the table will be filled up by code below
+		table.setModel(new DefaultTableModel(
+				new Object[][] {
+						{new Integer(1), null, "-", "-", "-"},
+				},
+				new String[] {
+						"Idx", "", "Start/Deadline", "End", "What to do?"
+				}
+				) {
+			@SuppressWarnings("rawtypes")
+			Class[] columnTypes = new Class[] {
+				Integer.class, Boolean.class, Object.class, Object.class, String.class
+			};
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(25);
+		table.getColumnModel().getColumn(1).setPreferredWidth(20);
+		table.getColumnModel().getColumn(2).setPreferredWidth(110);
+		table.getColumnModel().getColumn(3).setPreferredWidth(110);
+		table.getColumnModel().getColumn(4).setPreferredWidth(160);
+
+		popupStatus = new JPopupMenu();
+		addPopup(scrollPane, popupStatus);
+		scrollPane.setViewportView(table);
+		
+		String fileStatus = checkFilePermissions();		
+		executeCommand("list");
+		showStatus(fileStatus);
+	}
+
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+
+	@Override
+	public void runUI() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GuiMain window = new GuiMain();
+					window.frmDoit.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	class MyTableModel extends AbstractTableModel {
+		public static final long serialVersionUID = 8328597110205703514L;
+		public static final int COL_INDEX = 0;
+		public static final int COL_DONE = 1;
+		public static final int COL_START = 2;
+		public static final int COL_END = 3;
+		public static final int COL_TASKNAME = 4;
+
+		private String[] columnNames;
+		private List<Task> data;
+
+		public MyTableModel(List<Task> taskList){
+			columnNames = new String[]{"Idx", "", "Start/Deadline", "End", "Task"};
+			data = taskList;
+		}
+
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		public int getRowCount() {
+			//return data.length;
+			return data.size();
+		}
+
+		public String getColumnName(int col) {
+			return columnNames[col];
+		}
+
+		public Object getValueAt(int row, int col) {
+			Task task = data.get(row);
+
+			String start, end;
+			if(task.getType().equals(TaskType.TIMED)) {
+				start = dateTimeToLongerString(task.getStartTime());
+			} else if(task.getType().equals(TaskType.DEADLINE)) {
+				start = dateTimeToLongerString(task.getDeadline());
+			} else {
+				start = TABLE_EMPTY_DATE_FIELD;
+			}
+
+			if(task.getType().equals(TaskType.TIMED)) {
+				end = dateTimeToLongerString(task.getEndTime());
+			} else {
+				end = TABLE_EMPTY_DATE_FIELD;
+			}
+
+			switch(col){
+			case COL_INDEX:
+	    		return row+1;
+			case COL_DONE:
+				return task.isDone();
+			case COL_START:
+				return start;
+			case COL_END:
+				return end;
+			case COL_TASKNAME:
+				return task.getTaskName();
+			default:
+				assert false;
+			}
+			return null;
+		}
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		public Class getColumnClass(int c) {
+			return getValueAt(0, c).getClass();
+		}
+
+		public boolean isCellEditable(int row, int col) {
+			//Note that the data/cell address is constant,
+			//no matter where the cell appears on-screen.
+			if (col != COL_DONE) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		/*
+		 * Don't need to implement this method unless your table's
+		 * data can change.
+		 */
+		public void setValueAt(Object value, int row, int col) {
+			// TODO: must handle cell updated to tell the logic task has been marked as done!
+
+			Task task = data.get(row);
+
+			switch (col) {
+			case COL_INDEX:
+				assert false;
+				break;
+			case COL_DONE:
+				task.done((boolean) value);
+			case COL_START:
+				break;
+			case COL_END:
+				break;
+			case COL_TASKNAME:
+				break;
+			default:
+				assert false;
+			}
+
+			fireTableCellUpdated(row, col);
+		}
+	}
+
+	public void showTasksList(List<Task> taskList){		
+
+		table.setModel(new MyTableModel(taskList));
+		table.getColumnModel().getColumn(0).setMinWidth(25);
+		table.getColumnModel().getColumn(0).setMaxWidth(25);
+		table.getColumnModel().getColumn(1).setMinWidth(20);
+		table.getColumnModel().getColumn(1).setMaxWidth(20);
+		// Give more space to date: Test with 27 May 2009 10am to 27 May 2009 10pm
+		table.getColumnModel().getColumn(2).setMinWidth(170);
+		table.getColumnModel().getColumn(2).setMaxWidth(170);
+		table.getColumnModel().getColumn(3).setMinWidth(170);
+		table.getColumnModel().getColumn(3).setMaxWidth(170);
+		//table.getColumnModel().getColumn(4).setPreferredWidth(160);
+		
+		table.getModel().addTableModelListener(new TableModelListener() {
+
+			@Override
+			public void tableChanged(TableModelEvent e) {
+				int row = e.getFirstRow();
+				int column = e.getColumn();
+				TableModel model = (TableModel) e.getSource();
+				if (column == MyTableModel.COL_DONE) {
+					boolean done = (boolean) model.getValueAt(row, column);
+					int index = row + 1;
+					if (done) {
+						executeCommand("done " + index);
+					} else {
+						executeCommand("undone " + index);
+					}
+				}
+			}
+
+		});
+	}
+
+	public void executeCommand(String text) {
+
+		// Call command parser
+		LogicToUi returnValue = sendCommandToLogic(text);
+
+		// Set command text box to empty
+		textCmd.setText("");
+
+
+
+		//int popupWidth = 300;
+		//int popupHeight = 60;
+
+		showStatus(returnValue.getString());
+
+		//popupStatus.setPopupSize(popupWidth, popupHeight);
+		//popupStatus.show(frmDoit, (frmDoit.getWidth() - popupWidth) / 2,
+		//		frmDoit.getHeight() - popupHeight + 5);
+
+		// Call command to refresh the table
+			showTasksList(sendCommandToLogic("refresh").getList());
+
+
+		if (returnValue.containsList()) {
+			showTasksList(returnValue.getList());
+		}
+
+	}
+
+	private void showStatus(String status) {
+		txtStatus
+				.setText("<html><table align=\"center\"><tr><td valign=\"middle\" align=\"center\"><font size=\"4\">"
+						+ status
+						+ " &nbsp;&nbsp;&nbsp;<a href=\"http://doit/undo\">undo</a></font></td></tr></table></html>");
+	}
+
+}
