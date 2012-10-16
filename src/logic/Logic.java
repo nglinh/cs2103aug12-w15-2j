502043eac6b6f498d673117f0d53e9cfe9645c2a
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm.mshome.net>
2012-10-16 21:21:48 +0800

    Logic: Can now do sorting. Just call sort + { type, done, start, end, name}. If no parameter, default is sort start. The sort command will refresh the list based on the previous list parameters.

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -2,6 +2,8 @@ package logic;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.NoSuchElementException;
 import java.util.Stack;
 
@@ -10,7 +12,10 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 import shared.LogicToUi;
+import shared.LogicToUi.SortStatus;
 import shared.SearchTerms;
+import shared.Task.SortByEndDate;
+import shared.Task.SortByStartDate;
 import shared.Task.TaskType;
 import storage.Database;
 import storage.NoMoreUndoStepsException;
@@ -21,24 +26,29 @@ import storage.Database.DB_File_Status;
 
 public class Logic {
 	public static enum CommandType {
-		ADD, DELETE, LIST, SEARCH, SEARCH_PARTIAL, UNDO, FILE_STATUS, REFRESH, DONE, UNDONE,
+		ADD, DELETE, LIST, SEARCH, SEARCH_PARTIAL, UNDO, FILE_STATUS, REFRESH, DONE, UNDONE, SORT
 	};
+
 
 	public static Database dataBase = new Database();
 	private static ArrayList<Task> lastShownToUI = new ArrayList<Task>();
-	private static String latestRefreshCommandForGUI = "list";
+	private static String latestRefreshCommandForUI = "list";
+
 	private static String latestCommandFromUI = null;
 	private static Stack<String> undoHistory = new Stack<String>();
 
+	private static Comparator<Task> latestSorter = new SortByStartDate();
+	private static SortStatus latestSorting =  SortStatus.START;
+	private static String latestSortCommand = "sort start";
 
 
 	public static LogicToUi uiCommunicator(String command) {
 		LogicToUi feedback;
 		try {
-			
+
 			latestCommandFromUI = new String(command);
 			CommandType commandType = parseCommand(command);
-			
+
 			String arguments = command.replaceFirst(command.trim().split(" ")[0], "").trim();
 			feedback = executeCommand(commandType, arguments);
 		} catch (NoSuchCommandException e) {
@@ -51,15 +61,15 @@ public class Logic {
 	private static CommandType parseCommand(String command)
 			throws NoSuchCommandException {
 		String commandSyntax = command.trim().split(" ")[0];
-		
+
 		CommandType typeOfCommand = determineCommandType(commandSyntax);
 		return typeOfCommand;
 	}
 
 	private static CommandType determineCommandType(String string)
 			throws NoSuchCommandException {
-		
-		
+
+
 		switch (string.toLowerCase()) {
 		case "add":
 			//Fallthrough
@@ -71,24 +81,27 @@ public class Logic {
 			//Fallthrough
 		case "del" :
 			return CommandType.DELETE;
-		
+
 		case "list":
 			//Fallthrough
 		case "l" :
 			//Fallthrough
 		case "ls" :
 			return CommandType.LIST;
-		
+
 		case "search":
 			return CommandType.SEARCH;
 		case "searchpartial":
 			return CommandType.SEARCH_PARTIAL;
-			
+
 		case "undo":
 			//Fallthrough
 		case "u" :
 			return CommandType.UNDO;
-		
+
+		case "sort":
+			return CommandType.SORT;
+
 		case "filestatus":
 			return CommandType.FILE_STATUS;
 		case "refresh":
@@ -104,8 +117,8 @@ public class Logic {
 
 	private static LogicToUi executeCommand(CommandType commandType,
 			String arguments) {
-		
-	
+
+
 		switch (commandType) {
 		case ADD:
 			return addTask(arguments);
@@ -127,16 +140,89 @@ public class Logic {
 			return done(arguments, true);
 		case UNDONE: 
 			return done(arguments, false);
+		case SORT:
+			return sort(arguments);
 		default:
 			return null;
 		}
 	}
-	
+
+	private static LogicToUi sort(String arguments) {
+
+		if(arguments.length() == 0) {			
+			arguments = "start";
+		}
+
+		boolean needToReverse = false;
+		if(arguments.contains("reverse")) {
+			needToReverse = true;
+		}
+
+		Comparator<Task> sorter = null;
+
+		//Refresh the list first before sorting
+		LogicToUi fromListCommand = uiCommunicator(latestRefreshCommandForUI);
+		SearchTerms searchFilters = fromListCommand.getFilters();
+		String listStatusMsg = fromListCommand.getString();
+
+		String statusMsg = null;
+
+		if(arguments.contains("type")){
+			sorter = new Task.SortByType();
+			statusMsg = "sorted by Type";
+			latestSortCommand = "sort " + arguments;
+			latestSorting = SortStatus.TYPE;
+
+		} else if(arguments.contains("done")) {
+			sorter = new Task.SortByDone();
+			statusMsg = "sorted by Done";
+			latestSortCommand = "sort " + arguments;
+			latestSorting = SortStatus.DONE;
+
+		} else if (arguments.contains("start")) {
+			sorter = new SortByStartDate();
+			statusMsg = "sorted by Start Date/Deadline";
+			latestSortCommand = "sort " + arguments;
+			latestSorting = SortStatus.START;
+
+		} else if (arguments.contains("end")) {
+			sorter = new SortByEndDate();
+			statusMsg = "sorted by End Date/Deadline";
+			latestSortCommand = "sort " + arguments;
+			latestSorting = SortStatus.END;
+
+		} else if (arguments.contains("name")) {
+			sorter = new Task.SortByName();
+			statusMsg = "sorted by Name";
+			latestSortCommand = "sort " + arguments;
+			latestSorting = SortStatus.NAME;
+
+		} else {
+			sorter = latestSorter;
+			statusMsg = "Incorrect parameter for sort command";
+		}
+
+
+		latestSorter = sorter;
+		if(needToReverse){
+			sorter = Collections.reverseOrder(sorter);
+		}
+
+		Collections.sort(lastShownToUI, sorter);
+		String appendedSortStatus = listStatusMsg + ", " + statusMsg;
+
+		if(searchFilters == null){
+			return new LogicToUi(lastShownToUI, appendedSortStatus, latestSorting);
+		} else {
+			return new LogicToUi(lastShownToUI, appendedSortStatus, searchFilters, latestSorting);
+		}
+	}
+
 	private static void pushCommandToUndoHistoryStack() {
 		if(latestCommandFromUI == null) {
 			return;
 		}
-		
+
 		undoHistory.push(latestCommandFromUI);
 	}
 	private static LogicToUi done(String arguments, boolean newDoneStatus) {
@@ -144,30 +230,30 @@ public class Logic {
 			return new LogicToUi(
 					"Sorry this index number you provided is not valid. Please try again with a correct number or refresh the list.");
 		}
-		
+
 		int index;
 
-		
+
 		try {
 			index = Integer.parseInt(arguments);
 			index--; //Since arraylist index starts from 0
-			
+
 			if((index < 0) || ((index  +  1) > lastShownToUI.size()) ) {
 				throw new NoSuchElementException();
 			}
-			
+
 			int serial = lastShownToUI.get(index).getSerial();
-			
+
 			Task toBeUpdated = dataBase.locateATask(serial);
 			boolean oldDoneStatus = toBeUpdated.isDone();
-			
+
 			toBeUpdated.done(newDoneStatus);
 			dataBase.update(serial, toBeUpdated);
-			
+
 			pushCommandToUndoHistoryStack();
 
 			String taskDetails = taskToString(toBeUpdated);
-			
+
 			if(oldDoneStatus == newDoneStatus) {
 				return new LogicToUi(taskDetails + " has been already been marked as done/undone.");
 			} else if(newDoneStatus == true) {
@@ -188,52 +274,53 @@ public class Logic {
 	}
 
 	private static LogicToUi refresh() {
-		return uiCommunicator(latestRefreshCommandForGUI);
+
+		return uiCommunicator(latestSortCommand);
 	}
 
 	//Can only search by keywords for now
 	private static LogicToUi search(String arguments) {
-		
+
 		LogicToUi toBeSent = searchPartial(arguments);
-		
+
 		lastShownToUI = toBeSent.getList();
-		latestRefreshCommandForGUI = new String(latestCommandFromUI);
-		
+		latestRefreshCommandForUI = new String(latestCommandFromUI);
+
 		return toBeSent;
 	}
-	
+
 	//Can only search by keywords for now, allow GUI to show suggestions without affecting state
 	private static LogicToUi searchPartial(String arguments) {
-		
+
 		if(arguments.length() == 0) {
 			return new LogicToUi(
 					"No search terms specified.");
 		}
 		String[] keywords = arguments.split(" ");
-		
+
 		SearchTerms terms = new SearchTerms(keywords);
 		ArrayList<Task> results = dataBase.search(terms);
-		
+
 		String statusMsg = "You have searched for ";
-		
+
 		for(String keyword : keywords) {
 			statusMsg += " \"" + keyword + "\" ";
 		}
-		
+
 		return new LogicToUi(results, statusMsg, terms);
 	}
 
 	private static LogicToUi undo() {
-		
-		
+
+
 		try {
 			if(dataBase.getUndoStepsLeft() == 0) {
 				throw new NoMoreUndoStepsException();
 			}
-			
+
 			dataBase.undo();
 			String status = "This command \"" + undoHistory.pop() + "\" has been undone"; 
-			
+
 			return new LogicToUi(status);
 		} catch (NoMoreUndoStepsException e) {
 			return new LogicToUi("You don't have any more undo steps left");
@@ -244,12 +331,12 @@ public class Logic {
 		} catch (WillNotWriteToCorruptFileException e) {
 			return new LogicToUi("File is corrupted. Please check :(.");
 		}
-		
+
 	}
 
 	private static LogicToUi checkFileStatus() {
 		DB_File_Status status = dataBase.getFileAttributes();
-		
+
 		if(status.equals(DB_File_Status.FILE_ALL_OK)) {
 			return new LogicToUi("Database file is ready!");
 		} else if(status.equals(DB_File_Status.FILE_READ_ONLY)) {
@@ -262,29 +349,29 @@ public class Logic {
 	}
 
 	private static LogicToUi list(String arguments) {
-		
+
 		if(arguments.length() == 0) {
-			
+
 			lastShownToUI = dataBase.readAll();
-			latestRefreshCommandForGUI = new String(latestCommandFromUI);
+			latestRefreshCommandForUI = latestCommandFromUI;
 			return new LogicToUi(lastShownToUI, "List of all tasks");
 		}
-		
+
 		SearchTerms filter;
-		
+
 		String statusMsg = "Listing based on these parameters: ";
-		
+
 		if(arguments.contains("overdue")){
 			DateTime startDate = new DateTime(Long.MIN_VALUE);
 			DateTime endDate = new DateTime(); 
-			
+
 			filter = new SearchTerms(false, true, false, false, false, startDate, endDate);
-			
+
 			ArrayList<Task> results = dataBase.search(filter);
 
 			lastShownToUI = results;
-			latestRefreshCommandForGUI = new String(latestCommandFromUI);
-			
+			latestRefreshCommandForUI = new String(latestCommandFromUI);
+
 			statusMsg += " \"overdue\" ";
 			return new LogicToUi(results, statusMsg, filter);
 		}
@@ -298,7 +385,7 @@ public class Logic {
 		boolean floating = false;
 		boolean today = false;
 		boolean tomorrow = false;
-		
+
 
 
 		for (String eachParam : parameters) {
@@ -327,51 +414,51 @@ public class Logic {
 				floating = true;
 				statusMsg += " \"floating\" ";
 			}
-			
+
 			if (eachParam.equals("today")) {
 				today = true;
 				statusMsg += " \"today\" ";
 			}
-			
+
 			if (eachParam.equals("tomorrow")) {
 				tomorrow = true;
 				statusMsg += " \"tomorrow\" ";
 			}
-			
+
 
 		}
 
 		if(today && tomorrow){
 			DateTime startDate = new DateTime().withTimeAtStartOfDay();
 			DateTime endDate = startDate.plusDays(1).withTime(23, 59, 59, 999); 
-			
+
 			filter = new SearchTerms(complete, incomplete, timed,
 					deadline, floating, startDate, endDate );
 		} else if (today){
-			
+
 			DateTime startDate = new DateTime().withTimeAtStartOfDay();
 			DateTime endDate = startDate.withTime(23, 59, 59, 999); 
-			
+
 			filter = new SearchTerms(complete, incomplete, timed,
 					deadline, floating, startDate, endDate );
-			
+
 		} else if(tomorrow ) {
 			DateTime startDate = new DateTime().plusDays(1).withTimeAtStartOfDay();
 			DateTime endDate = startDate.plusDays(1).withTime(23, 59, 59, 999); 
-			
+
 			filter = new SearchTerms(complete, incomplete, timed,
 					deadline, floating, startDate, endDate );
-			
+
 		} else {
 			filter = new SearchTerms(complete, incomplete, timed,
-				deadline, floating);
+					deadline, floating);
 		}
-		
-		
+
+
 		ArrayList<Task> results = dataBase.search(filter);
 
 		lastShownToUI = results;
-		latestRefreshCommandForGUI = new String(latestCommandFromUI);
+		latestRefreshCommandForUI = latestCommandFromUI;
 		return new LogicToUi(results, statusMsg, filter);
 	}
 
@@ -380,7 +467,7 @@ public class Logic {
 			return new LogicToUi(
 					"Sorry this index number or parameter you provided is not valid. Please try again with a correct number or refresh the list.");
 		}
-		
+
 
 		int index;
 
@@ -390,29 +477,29 @@ public class Logic {
 				pushCommandToUndoHistoryStack();
 				return new LogicToUi("All tasks that has ended before this moment have been deleted");
 			}
-			
+
 			if(arguments.equals("done")){
 				dataBase.deleteDone();
 				pushCommandToUndoHistoryStack();
 				return new LogicToUi("All completed tasks have been deleted");
 			}
-			
+
 			if(arguments.equals("all")){
 				dataBase.deleteAll();
 				pushCommandToUndoHistoryStack();
 				return new LogicToUi("All tasks have been deleted");
 			}
-			
-			
+
+
 			index = Integer.parseInt(arguments);
 			index--; //Since arraylist index starts from 0
-			
+
 			if((index < 0) || ((index  +  1) > lastShownToUI.size()) ) {
 				throw new NoSuchElementException();
 			}
-			
+
 			int serial = lastShownToUI.get(index).getSerial();
-			
+
 			Task toBeDeleted = dataBase.locateATask(serial);
 			dataBase.delete(serial);
 			pushCommandToUndoHistoryStack();
@@ -434,7 +521,7 @@ public class Logic {
 	}
 
 	private static String taskToString(Task toBeConverted) {
-		
+
 		if(toBeConverted.isTimedTask()) {
 			return("Timed task " + "\"" + toBeConverted.getTaskName() + "\"" + " from " + dateToString(toBeConverted.getStartDate()) + " to " + dateToString(toBeConverted.getEndDate()));
 		} else if(toBeConverted.isDeadlineTask()) {
