5accbd7a9851f9b0a3521c9459668f28b73f18cf
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm>
2012-10-28 00:37:26 +0800

    help.xml: Shifted directory to /src/resource

---
+++ src/main/ui/CliHelpText.java
@@ -0,0 +1,142 @@
+package main.ui;
+
+/**  
+ * CliHelp.java
+ * A class holding the help contents for Cli
+ * @author  Yeo Kheng Meng
+ */
+
+public class CliHelpText {
+
+	private static final String MESSAGE_NO_SUCH_COMMAND_AVAILABLE = "This command \"%1%s\" is not supported by DoIT.";
+	
+	CliHelpText() {
+
+	}
+
+	public String help() {
+		String helpCommands = 
+				"Type \"help [command]\" for detailed usage for \"command\"" + "\n" +
+						"add" + "\n" + 
+						"list" + "\n" + 
+						"delete" + "\n" + 
+						"edit" + "\n" + 
+						"postpone" + "\n" + 
+						"done" + "\n" + 
+						"undone" + "\n" + 
+						"undo" + "\n" +
+						"refresh" + "\n";
+
+		return helpCommands;
+	}
+
+	public String detailedCommandHelp(String commandHelp) {
+
+		commandHelp = commandHelp.trim();
+		commandHelp = commandHelp.toLowerCase();
+		String helpText = null;
+
+		switch(commandHelp) {
+		case "add" :
+			helpText = add();
+			break;
+		case "list" :
+			helpText = list();
+			break; 
+		case "delete" :
+			helpText = delete();
+			break;
+		case "edit" :
+			helpText = edit();
+			break;
+		case "postpone" :
+			helpText = postpone();
+			break;
+		case "done" :
+			helpText = done();
+			break;
+		case "undone" :
+			helpText = undone();
+			break;
+		case "undo" :
+			helpText = undo();
+			break;
+		case "refresh" :
+			helpText = refresh();
+			break;
+		default :
+			helpText =  String.format(MESSAGE_NO_SUCH_COMMAND_AVAILABLE, commandHelp);
+		}
+
+		return helpText;
+	}
+
+	private String refresh() {
+		String text = 
+				"";
+		return text;
+	}
+
+	private String undo() {
+		String text = 
+				"Undos the last change you made.\n" +
+				"Usage: undo\n\n" +
+				"Example: undo\n\n" +
+				"Advised to call a list operation after every change to database";
+		
+		return text;
+	}
+
+	private String undone() {
+		String text = 
+				"Marks the task specified by index as undone.\n" +
+				"Usage: undone [index]\n\n" +
+				"To get \"index\" number, use the list command.\n" +
+				"\"index\" will be based on last shown list.\n\n" +
+				"Example: undone 5";
+				
+		return text;
+	}
+
+	private String done() {
+		String text = 
+				"Marks the task specified by index as done.\n" +
+				"Usage: done [index]\n\n" +
+				"To get \"index\" number, use the list command.\n" +
+				"\"index\" will be based on last shown list.\n\n" +
+				"Example: done 5";;
+				
+		return text;
+	}
+
+	private String postpone() {
+		String text = 
+				"";
+		return text;
+	}
+
+	private String edit() {
+		String text = 
+				"";
+		return text;
+	}
+
+	private String delete() {
+		String text = 
+				"";
+		return text;
+	}
+
+	private String list() {
+		String text = 
+				"";
+		return text;
+	}
+
+	private String add() {
+		String text = 
+				"";
+		return text;
+	}
+
+}
---
+++ src/main/ui/UI.java
@@ -0,0 +1,71 @@
+package main.ui;
+
+/**  
+ * UI.java 
+ * An abstract class for all the UIs.
+ * @author  Yeo Kheng Meng
+ */ 
+
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import main.shared.LogicToUi;
+import main.logic.Logic;
+
+public abstract class UI {
+	private final String LINE_DATE_FORMAT = "dd-MMM-yy hh:mma";
+	private final String LINE_DATE_LONGER_FORMAT = "EEE dd-MMM-yyyy hh:mma";
+	
+	private final DateTimeFormatter LINE_DATE_FORMATTER = DateTimeFormat.forPattern(LINE_DATE_FORMAT);
+	private final DateTimeFormatter LINE_DATE_LONGER_FORMATTER = DateTimeFormat.forPattern(LINE_DATE_LONGER_FORMAT);
+	
+	protected static final String COMMAND_CHECK_FILE_PERMISSIONS = "fileStatus";
+
+	
+	private Logic logic = Logic.getInstance();
+	private Hint hint = Hint.getInstance();
+	
+	//This is the first method that will run the UI after it is constructed. DoITstart will run this.
+	public abstract void runUI();
+	
+	protected LogicToUi sendCommandToLogic(String command) {
+		return logic.uiCommunicator(command);
+	}
+	
+	
+	protected String dateTimeToString(DateTime toBeConverted) {
+		return LINE_DATE_FORMATTER.print(toBeConverted);
+	}
+	
+	protected String dateTimeToLongerString(DateTime toBeConverted) {
+		return LINE_DATE_LONGER_FORMATTER.print(toBeConverted);
+	}
+	
+	protected String currentTimeInLongerForm() {
+		return dateTimeToLongerString((new DateTime()));
+	}
+	
+	protected String checkFilePermissions() {
+		LogicToUi filePermissions = sendCommandToLogic(COMMAND_CHECK_FILE_PERMISSIONS);
+		return filePermissions.getString();
+	}
+	
+	
+	protected String getHTMLHelp(String command){
+		return hint.helpForThisCommandHTML(command);
+	}
+	
+	protected String getNoHTMLHelp(String command){
+		return hint.helpForThisCommandNoHTML(command);
+	}
+	
+	protected void exit(){
+		System.exit(0);
+	}
+
+
+
+
+}
