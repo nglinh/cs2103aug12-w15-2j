9dbb181aece1551d9940591e15cccc8e8a63f5ae
mrlinh <mrlinh@mrlinh-HP.comp.nus.edu.sg>
2012-10-29 13:01:51 +0800

    _ Refactored AddParser(add javadoc) _ Updated natty to 0.7(need to reimport) _ Updated logic test cases(Please follow the order convention inside) _ Add and Edit will return the recently changed task's serial ID to UI.

---
+++ src/main/logic/CannotParseDateException.java
@@ -0,0 +1,8 @@
+package main.logic;
+
+public class CannotParseDateException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public CannotParseDateException (){
+		super();
+	}
+}
