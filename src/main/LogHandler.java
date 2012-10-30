42a5cef2059b174468a8e7518246534a1f4a7a65
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm.mshome.net>
2012-10-30 23:29:14 +0800

    DoItStart, loghandler, FileMgmt: Add/modified logging

---
+++ src/main/LogHandler.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+public class LogHandler {
+
+
+	private static final String LOG_FILE_NAME = "DoItLog.log";
+	private static FileHandler fh;
+	private static Logger theOneLogger = null;
+
+
+	public static Logger getLogInstance(){
+		if(theOneLogger == null){
+			try {
+				theOneLogger = Logger.getLogger("DoItLogger");
+
+				
+				//To disable logging to Standard Error
+				theOneLogger.setUseParentHandlers(false);
+				
+				
+				theOneLogger.addHandler(getFileHandler());
+			} catch (SecurityException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return theOneLogger;
+	}
+
+
+	private static FileHandler getFileHandler() throws SecurityException, IOException{	
+			if(fh == null){
+				fh = new FileHandler(LOG_FILE_NAME);
+			}
+		return fh;
+	}
+	
+
+
+
+
+
+}
---
+++ src/main/ui/UndatedTaskListRenderer.java
@@ -0,0 +1,56 @@
+package main.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import main.shared.Task;
+
+public class UndatedTaskListRenderer {
+	
+	private List<Task> taskList;
+	
+	private List<Integer> indexList;
+	
+	public UndatedTaskListRenderer(List<Task> taskList){
+		this.taskList = taskList;
+		
+		indexList = new ArrayList<Integer>();
+	}
+	
+	public String render(){
+		
+		StringBuffer sb = new StringBuffer();
+		for(Task task : taskList){
+			if (task.getType() == Task.TaskType.FLOATING){
+				sb.append(renderTask(task));
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+	public String renderTask(Task t){
+		StringBuffer sb = new StringBuffer();
+		
+		int taskIndex = taskList.indexOf(t)+1;
+		String taskName = t.getTaskName();
+		
+		sb.append("<table cellpadding=0 cellspacing=0 class=\"taskbox\" width=100%>");
+		sb.append("<tr><td rowspan=2><font size=5>"+HTMLEncoder.encode(taskName)+"</font></td><td width=1 align=center>" + taskIndex + "</td></tr>");
+		//sb.append("<tr><td valign=top align=center><input type=checkbox></td></tr>");
+		if(t.isDone()){
+			sb.append("<tr><td valign=top align=center><input type=checkbox checked></td></tr>");
+		}else{
+			sb.append("<tr><td valign=top align=center><input type=checkbox></td></tr>");
+		}
+		sb.append("</table>");
+		
+		indexList.add(taskIndex);
+		
+		return sb.toString();
+	}
+	
+	public List<Integer> getIndexList(){
+		return indexList;
+	}
+
+}
