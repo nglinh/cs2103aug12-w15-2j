82ae04c8f4e76f498e4becf764c6e1df0fb17282
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm.comp.nus.edu.sg>
2012-10-31 14:09:00 +0800

    Cli: Renamed logging message a little

---
+++ src/main/ui/CliWithJline.java
@@ -0,0 +1,172 @@
+package main.ui;
+
+/**  
+ * CliWithJline.java
+ * A class for managing the Cli interface that has features like Tab Completion and Command History
+ * @author  Yeo Kheng Meng
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jline.console.ConsoleReader;
+import jline.console.completer.Completer;
+import jline.console.completer.ArgumentCompleter;
+import jline.console.completer.StringsCompleter;
+
+
+public class CliWithJline extends Cli{
+
+	private static final int TABLE_MINIMUM_DESCRIPTION_SIZE = 16;
+	private static final String TABLE_HEADER_FILLER = " ";
+	private static final String TABLE_HEADER_END = " |";
+	private static final String TABLE_HEADER_BEGINNING = "|Idx| |  Start/Deadline   |        End        |      What to Do?";
+	private static final String TABLE_END_CHARACTER = "-+";
+	private static final String TABLE_BORDER_FILLER = "-";
+	private static final String TABLE_ROW_DEMARCATION_BEGIN = "+---+-+-------------------+-------------------+-";
+	private static final String TABLE_TOP_AND_BOTTOM_BEGIN = "+-----------------------------------------------";
+	private static final String MESSAGE_CLI_CUSTOM = "Includes Tab Completion\nRun DoIt with -clisafe if you experience problems";
+	protected static final String MESSAGE_INITIAL_HELP_OFFER_JLINE = "Press Tab or type \"help\" for a list of commands.";
+
+
+	private static final Completer baseCommandList = new StringsCompleter (commandList);
+
+	private static final Completer helpCommand = new StringsCompleter(new String [] {"help"});
+	private static final Completer helpArguments = baseCommandList;
+
+	private static final Completer listCommand = new StringsCompleter(new String [] {"list", "ls", "l"});
+	private static final Completer listArguments = new StringsCompleter(new String [] {"done", "undone", "timed", "deadline", "floating" , "today", "tomorrow", "overdue"});
+
+	private static final Completer delCommand = new StringsCompleter(new String [] {"del", "delete", "de"});
+	private static final Completer delArguments = new StringsCompleter(new String [] {"all", "over", "done"});
+
+	private static final Completer sortCommand = new StringsCompleter(new String [] {"sort"});
+	private static final Completer sortArguments = new StringsCompleter(new String [] {"type", "done", "start", "end", "name", "reverse"});
+
+	List<Completer> listSet = new ArrayList<Completer>();
+	ArgumentCompleter listArgCmp;
+
+	List<Completer> helpSet = new ArrayList<Completer>();
+	ArgumentCompleter helpArgCmp;
+
+	List<Completer> delSet = new ArrayList<Completer>();
+	ArgumentCompleter delArgCmp;
+
+	List<Completer> sortSet = new ArrayList<Completer>();
+	ArgumentCompleter sortArgCmp;
+
+	ConsoleReader console;
+
+	@Override
+	public void runUI(){
+
+		System.out.println(MESSAGE_WELCOME_TO_DO_IT);
+		System.out.println(MESSAGE_CLI_CUSTOM);
+		System.out.println();
+		
+		
+
+		try {
+			log.info("Before instantiate console reader");
+			console = new ConsoleReader();
+			log.info("After instantiate console reader");
+			
+			configureArguments();
+
+			console.println(checkFilePermissions() + "\n");
+			console.println(MESSAGE_INITIAL_HELP_OFFER_JLINE);
+
+			console.setPrompt(MESSAGE_NEXT_COMMAND);
+
+			String lineFromInput;
+			while(true)	{
+				lineFromInput = console.readLine();
+				log.info("Received this command \"" + lineFromInput + "\"");
+				
+				getConsoleSizeAndAdjustOutput();
+				String consoleOut = processInput(lineFromInput);
+				log.info("Output this string \"" + consoleOut + "\"");
+				
+				console.println();
+				console.println(consoleOut);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.warning("Probably issue with Jline " + e);
+		}
+
+	}
+
+	private void getConsoleSizeAndAdjustOutput(){
+		//DoIT is only guaranteed to display properly for the minimum description width, 
+		//below that, the header for the description will be too small.
+		if(consoleWidth == console.getTerminal().getWidth()){
+			return;
+		} else {
+			consoleWidth = console.getTerminal().getWidth();
+			log.info("New console Width detected to be " + consoleWidth);
+		}
+
+		TABLE_DESCRIPTION_ALLOWANCE = consoleWidth - TABLE_SIZE_OF_PRE_TASK_DESCRIPTION;
+		
+		log.info("Table Description Allowance detected to be " + TABLE_DESCRIPTION_ALLOWANCE);
+		if(TABLE_DESCRIPTION_ALLOWANCE < TABLE_MINIMUM_DESCRIPTION_SIZE) {
+			TABLE_DESCRIPTION_ALLOWANCE = TABLE_MINIMUM_DESCRIPTION_SIZE;
+		}
+		
+		log.info("Table Description Allowance set to be " + TABLE_DESCRIPTION_ALLOWANCE);
+
+		TABLE_TOP_AND_BOTTOM = TABLE_TOP_AND_BOTTOM_BEGIN;
+		TABLE_ROW_DEMARCATION = TABLE_ROW_DEMARCATION_BEGIN;
+
+		for(int i = 0; i < TABLE_DESCRIPTION_ALLOWANCE; i++){
+			TABLE_TOP_AND_BOTTOM += TABLE_BORDER_FILLER;
+			TABLE_ROW_DEMARCATION += TABLE_BORDER_FILLER;
+		}
+
+		TABLE_TOP_AND_BOTTOM += TABLE_END_CHARACTER;
+		TABLE_ROW_DEMARCATION += TABLE_END_CHARACTER;
+
+		TABLE_HEADER = TABLE_HEADER_BEGINNING;
+		for(int i = 0; i < TABLE_DESCRIPTION_ALLOWANCE - 16; i++){
+			TABLE_HEADER += TABLE_HEADER_FILLER;
+		}
+
+		TABLE_HEADER += TABLE_HEADER_END;
+
+
+		TABLE_DESCRIPTION_PAD = "%-" + TABLE_DESCRIPTION_ALLOWANCE + "s";
+	}
+
+	private void configureArguments() {
+		listSet.add(listCommand);
+		listSet.add(listArguments);
+		listArgCmp = new ArgumentCompleter(listSet);
+
+		helpSet.add(helpCommand);
+		helpSet.add(helpArguments);
+		helpArgCmp = new ArgumentCompleter(helpSet);
+
+		delSet.add(delCommand);
+		delSet.add(delArguments);
+		delArgCmp = new ArgumentCompleter(delSet);
+
+		sortSet.add(sortCommand);
+		sortSet.add(sortArguments);
+		sortArgCmp = new ArgumentCompleter(sortSet);			
+
+
+		console.addCompleter(baseCommandList);
+		console.addCompleter(listArgCmp);
+		console.addCompleter(helpArgCmp);
+		console.addCompleter(delArgCmp);
+		console.addCompleter(sortArgCmp);
+	}
+
+
+
+
+
+}
---
+++ src/main/ui/UndatedTaskListRenderer.java
@@ -0,0 +1,71 @@
+package main.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sun.util.logging.resources.logging;
+import main.shared.Task;
+
+public class UndatedTaskListRenderer {
+	
+	private List<Task> taskList;
+	
+	private List<Integer> indexList;
+	
+	private int highlightSerial = -1;
+	
+	public UndatedTaskListRenderer(List<Task> taskList){
+		this.taskList = taskList;
+		
+		indexList = new ArrayList<Integer>();
+	}
+	
+	public void setHighlightSerial(int highlightSerial) {
+		this.highlightSerial = highlightSerial;
+	}
+	
+	public String render(){
+		
+		StringBuffer sb = new StringBuffer();
+		for(Task task : taskList){
+			if (task.getType() == Task.TaskType.FLOATING){
+				sb.append(renderTask(task));
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+	public String renderTask(Task t){
+		StringBuffer sb = new StringBuffer();
+		
+		int taskIndex = taskList.indexOf(t)+1;
+		String taskName = t.getTaskName();
+		
+		String cssClass = "taskbox";
+		
+		if(highlightSerial == t.getSerial()){
+			cssClass = "taskboxhighlight";
+			sb.append("<div class=separatorfirst><a name=\"highlight\"></a></div>");
+		}
+		
+		sb.append("<table cellpadding=0 cellspacing=0 class=\""+cssClass+"\" width=100%>");
+		sb.append("<tr><td rowspan=2><font size=5>"+HTMLEncoder.encode(taskName)+"</font></td><td width=1 align=center>" + taskIndex + "</td></tr>");
+		//sb.append("<tr><td valign=top align=center><input type=checkbox></td></tr>");
+		if(t.isDone()){
+			sb.append("<tr><td valign=top align=center><input type=checkbox checked></td></tr>");
+		}else{
+			sb.append("<tr><td valign=top align=center><input type=checkbox></td></tr>");
+		}
+		sb.append("</table>");
+		
+		indexList.add(taskIndex);
+		
+		return sb.toString();
+	}
+	
+	public List<Integer> getIndexList(){
+		return indexList;
+	}
+
+}
