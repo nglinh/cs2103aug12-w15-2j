b64b0806be605cf794a0184bab9e5605f7f32f22
Kai Yao <Kai Yao@KaiYao-PC.comp.nus.edu.sg>
2012-11-01 17:11:24 +0800

    ui.GuiMain: highlight color now more obvious, also highlight dates with tasks on calendar

---
+++ src/main/logic/UndeterminedTTypeException.java
@@ -0,0 +1,13 @@
+package main.logic;
+
+public class UndeterminedTTypeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	UndeterminedTTypeException(){
+		super();
+	}
+	
+}
