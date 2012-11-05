910b534614b924df438db268c5916816dff80a64
mrlinh <mrlinh@mrlinh-HP>
2012-11-05 22:54:33 +0800

    Moved lastShownToUi to another class so as all can refer to. Refactored AddParser more. Changed Postpone, done and undone parsers to return task object instead of integer. Removed 2 depricated methods in commandhandler since lastshowntoui is pushed to new file alr

--- src/main/logic/CommandHandler.java
+++ src/main/logic/CommandHandler.java
@@ -1,6 +1,5 @@
 package main.logic;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,94 +16,82 @@ import main.storage.Database;
 
 public abstract class CommandHandler {
 
-	protected static final String ERROR_CANNOT_PARSE_DATE = "One or more field(s) expects time component. However,"
+	protected static final String ERROR_CANNOT_PARSE_DATE = "One or more field(s)"
+			+ " expects time component. However,"
 			+ "either time component is missing, or DoIt! could not parse it :(."
 			+ "Please check your input";
-	protected static final String ERROR_IO = "Something is wrong with the file. I cannot write to it. Please check your file permissions.";
-	protected static final String ERROR_FILE_CORRUPTED = "File is corrupted. Please rectify the problem or delete the database file and restart DoIT. :(";
-	protected static final String ERROR_TASKDES_EMPTY = "Task name cannot be empty. Please check again :(.";
-	protected static final String ERROR_INDEX_NUMBER_NOT_VALID = "Sorry you did not provide an index number or the number you provided is not valid. Please try again with a correct number or refresh the list.";
-	
-	
+	protected static final String ERROR_IO = "Something is wrong with the file."
+			+ " I cannot write to it. Please check your file permissions.";
+	protected static final String ERROR_FILE_CORRUPTED = "File is corrupted. Please"
+			+ " rectify the problem or delete the database file and restart DoIT. :(";
+	protected static final String ERROR_TASKDES_EMPTY = "Task name cannot be empty."
+			+ " Please check again :(.";
+	protected static final String ERROR_INDEX_NUMBER_NOT_VALID = "Sorry you did not"
+			+ " provide an index number or the number you provided is not valid."
+			+ " Please try again with a correct number or refresh the list.";
+
 	private static LinkedList<String> undoMsgHistory = new LinkedList<String>();
 	private static LinkedList<List<Task>> undoClones = new LinkedList<List<Task>>();
-	
+
 	protected static Database dataBase = Database.getInstance();
 	protected static String latestCommandFromUI = null;
-	
-	protected static List<Task> lastShownToUI = new ArrayList<Task>();
+
+	protected static List<Task> lastShownToUI = LastShownToUI.getInstance();
 	protected static String latestRefreshCommandForUI = "list";
-	
+
 	protected static Comparator<Task> latestSorter = new SortByStartDate();
 	protected static SortStatus latestSorting = SortStatus.START;
 	protected static String latestSortArgument = "start";
 
 	protected LogicToUi feedback;
-	
+
 	public static final int MAX_UNDO_STEPS = 50;
-	
+
 	private Logic logic = Logic.getInstance();
 
-	
 	public CommandHandler(String arguments) {
 	}
-	
-	protected LogicToUi sendCommandToLogicAgain(String commandToSend){
+
+	protected LogicToUi sendCommandToLogicAgain(String commandToSend) {
 		return logic.uiCommunicator(commandToSend);
 	}
-	
-	
-	@Deprecated
-	public static int getSizeofLastShownToUiList(){
-		return lastShownToUI.size();
-	}
-	
-	@Deprecated
-	public static Task getTaskFromLastShownToUi(int index){
-		return lastShownToUI.get(index);
-	}
-
 
 	public abstract LogicToUi execute();
-	
-	protected void pushUndoStatusMessageAndTaskList(String undoMsg, List<Task> currentCopy){
+
+	protected void pushUndoStatusMessageAndTaskList(String undoMsg,
+			List<Task> currentCopy) {
 		undoMsgHistory.push(undoMsg);
 		undoClones.push(currentCopy);
 
-		while(undoMsgHistory.size() > MAX_UNDO_STEPS) {
+		while (undoMsgHistory.size() > MAX_UNDO_STEPS) {
 			undoMsgHistory.removeFirst();
 		}
-		
 
-		while(undoClones.size() > MAX_UNDO_STEPS) {
+		while (undoClones.size() > MAX_UNDO_STEPS) {
 			undoClones.removeFirst();
 		}
-		
+
 	}
-	
-	
-	protected List<Task> getCurrentTaskList(){
+
+	protected List<Task> getCurrentTaskList() {
 		return dataBase.readAll();
 	}
-	
-	
-	protected List<Task> peekUndoClones(){
+
+	protected List<Task> peekUndoClones() {
 		return undoClones.peek();
 	}
-	
-	protected void popUndoClones(){
+
+	protected void popUndoClones() {
 		undoClones.pop();
 	}
-	
-	protected int undoStepsRemaining(){
+
+	protected int undoStepsRemaining() {
 		return undoClones.size();
 	}
-	
+
 	protected String popAndGetPrevUndoMsg() {
 		return undoMsgHistory.pop();
 	}
-
-
 
 	protected String taskToString(Task toBeConverted) {
 		if (toBeConverted.isTimedTask()) {
