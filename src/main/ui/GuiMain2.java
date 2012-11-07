03bef6878e9d382d155eefc7114f6810ea8d9f09
Kai Yao <Kai Yao@KaiYao-PC.lan>
2012-11-08 01:15:28 +0800

    ui.GuiMain2: added the list view as a separate "card" to agenda view UI window.

--- src/main/ui/GuiMain2.java
+++ src/main/ui/GuiMain2.java
@@ -13,6 +13,9 @@ import javax.swing.JTextField;
 import javax.swing.JToggleButton.ToggleButtonModel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.Document;
 import javax.swing.text.Element;
@@ -24,7 +27,10 @@ import javax.swing.text.html.StyleSheet;
 import main.LogHandler;
 import main.shared.LogicToUi;
 import main.shared.Task;
+import main.shared.Task.TaskType;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
@@ -35,13 +41,23 @@ import java.util.logging.Logger;
 import javax.swing.JPanel;
 import javax.swing.event.HyperlinkListener;
 import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 
 import org.joda.time.DateTime;
+
+import com.joestelmach.natty.DateGroup;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
 import javax.swing.JToolBar;
 import javax.swing.JButton;
 import javax.swing.JSeparator;
@@ -51,6 +67,12 @@ import javax.swing.ImageIcon;
 import java.awt.CardLayout;
 
 public class GuiMain2 extends GuiCommandBox{
+	
+	private static final int TABLE_COLUMN_WIDTH_CHECKBOX = 20;
+	private static final String TABLE_COLUMN_WIDTH_DATE_MAX_TEXT = "WMW 00 MWM 0000 23:59pm";
+	private static final String TABLE_COLUMN_WIDTH_INDEX_MAX_TEXT = "9999";
+	private static final int TABLE_COLUMN_WIDTH_EXTRA = 10;
+	private static final String TABLE_EMPTY_DATE_FIELD = "";
 
 	Logger log = LogHandler.getLogInstance();
 
@@ -91,6 +113,12 @@ public class GuiMain2 extends GuiCommandBox{
 	private JButton btnPreferences;
 	private JPanel panel_1;
 
+	private JScrollPane scrollPaneTable;
+
+	private JTable table;
+
+	private JScrollPane scrollPane;
+
 	/**
 	 * Launch the application.
 	 */
@@ -130,6 +158,7 @@ public class GuiMain2 extends GuiCommandBox{
 	 * Initialize the contents of the frame.
 	 * @wbp.parser.entryPoint
 	 */
+	@SuppressWarnings("serial")
 	protected void initialize() {
 		log.entering(this.getClass().getName(), "initialize");
 				
@@ -164,9 +193,25 @@ public class GuiMain2 extends GuiCommandBox{
 		toolBar.add(separator);
 		
 		tglbtnAgendaView = new JToggleButton("Agenda View");
+		tglbtnAgendaView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cl = (CardLayout)(panel_1.getLayout());
+				cl.show(panel_1, "agendaCard");
+				tglbtnAgendaView.setSelected(true);
+				tglbtnListView.setSelected(false);
+			}
+		});
 		toolBar.add(tglbtnAgendaView);
 		
 		tglbtnListView = new JToggleButton("List View");
+		tglbtnListView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CardLayout cl = (CardLayout)(panel_1.getLayout());
+				cl.show(panel_1, "listCard");
+				tglbtnAgendaView.setSelected(false);
+				tglbtnListView.setSelected(true);
+			}
+		});
 		toolBar.add(tglbtnListView);
 		
 		separator_1 = new JSeparator();
@@ -180,8 +225,12 @@ public class GuiMain2 extends GuiCommandBox{
 		frmDoit.getContentPane().add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(new CardLayout(0, 0));
 		
+		// *************************
+		// * Panel for Agenda View *
+		// *************************
+		
 		JSplitPane splitPane = new JSplitPane();
-		panel_1.add(splitPane, "name_133925802995198");
+		panel_1.add(splitPane, "agendaCard");
 		splitPane.setDividerLocation(350);
 		
 		scrollPaneDated = new JScrollPane();
@@ -216,6 +265,63 @@ public class GuiMain2 extends GuiCommandBox{
 		panel.add(txtCalendar, BorderLayout.SOUTH);
 		
         txtCalendar.setEditorKit(generateCalendarDocumentStyle());
+        
+        // ***********************
+        // * Panel for List View *
+        // ***********************
+        
+        scrollPane = new JScrollPane();
+
+		table = new JTable();
+		table.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				log.finest("Key pressed: " + arg0.getKeyCode());
+				if(arg0.getKeyCode() == java.awt.event.KeyEvent.VK_DELETE){
+					log.finer("Delete key pressed");
+					// Note: With every deletion, the index number changes!
+					// Therefore we must delete from the largest number backwards.
+					log.info("Deleting selected tasks");
+					int[] rowsToDelete = table.getSelectedRows();
+					Arrays.sort(rowsToDelete);
+					//System.out.println(Arrays.toString(rowsToDelete));
+					log.info("Rows to delete: "+ Arrays.toString(rowsToDelete));
+					for(int i = (rowsToDelete.length-1); i>=0; i--){
+						executeCommand("delete " + (rowsToDelete[i] + 1));
+					}
+				}
+			}
+		});
+		// Dummy table for WindowBuilder, the table will be filled up by code below
+		table.setModel(new DefaultTableModel(
+				new Object[][] {
+						{new Integer(1), null, "-", "-", "-"},
+				},
+				new String[] {
+						"Idx", "", "Start/Deadline", "End", "What to do?"
+				}
+				) {
+			@SuppressWarnings("rawtypes")
+			Class[] columnTypes = new Class[] {
+				Integer.class, Boolean.class, Object.class, Object.class, String.class
+			};
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(25);
+		table.getColumnModel().getColumn(1).setPreferredWidth(20);
+		table.getColumnModel().getColumn(2).setPreferredWidth(110);
+		table.getColumnModel().getColumn(3).setPreferredWidth(110);
+		table.getColumnModel().getColumn(4).setPreferredWidth(160);
+
+		scrollPane.setViewportView(table);
+		panel_1.add(scrollPane, "listCard");       
+        
+        // *************************
+        // * Panel for command box *
+        // *************************
         
         panelCmd = new JPanel();
 		frmDoit.getContentPane().add(panelCmd, BorderLayout.SOUTH);
@@ -299,7 +405,9 @@ public class GuiMain2 extends GuiCommandBox{
 	}
 	
 	public void update(LogicToUi returnValue){
-		showTasksList(sendCommandToLogic("refresh").getList(), returnValue.getLastChangedSerial());		
+		List<Task> refreshedList = sendCommandToLogic("refresh").getList();
+		showTasksList(refreshedList, returnValue.getLastChangedSerial());
+		showTasksListInTable(refreshedList);		
 		showStatus(returnValue.getString());
 		GuiUpdate.update(this);
 	}
@@ -498,6 +606,356 @@ public class GuiMain2 extends GuiCommandBox{
 				}
 			}
 		}
+	}
+	
+	private class TableChangedHandler implements TableModelListener {
+		@Override
+		public void tableChanged(TableModelEvent e) {
+			log.entering(this.getClass().getName(), "tableChanged");
+			log.info("Table has been modified");
+			
+			int row = e.getFirstRow();
+			int column = e.getColumn();
+			log.info(String.format("Cell at row %1d and col %2d has been modified", row, column));
+			
+			TableModel model = (TableModel) e.getSource();
+			if (column == MyTableModel.COL_DONE) {
+				log.info("Checkbox clicked");
+				boolean done = (boolean) model.getValueAt(row, column);
+				int index = row + 1;
+				if (done) {
+					//System.out.println("done " + index);
+					executeCommand("done " + index);
+				} else {
+					//System.out.println("undone " + index);
+					executeCommand("undone " + index);
+				}
+			}
+			if (column == MyTableModel.COL_START) {
+				log.info("Start date changed");
+				
+				String startTime = (String) model.getValueAt(row, column);
+				startTime = startTime.trim();
+				String endTime = (String) model.getValueAt(row, MyTableModel.COL_END);
+				endTime = endTime.trim();
+				int index = row + 1;
+				
+				log.finer(String.format("New start date %1s, new end date %2s", startTime, endTime));
+				
+				try{
+					if(startTime.isEmpty() && endTime.isEmpty()){
+						// Floating task
+						log.finer("Both start and end time empty");
+						executeCommand("update " + index +" -tofloating");
+					}else if(startTime.isEmpty() || endTime.isEmpty()){
+						log.finer("Either start or end time empty");
+						// Deadline task
+						String deadline;
+						if(startTime.isEmpty()){
+							log.finer("Start is empty, so we set deadline to endTime");
+							deadline = endTime;
+						}else{
+							log.finer("Start is not empty implies endTime is empty, so we set deadline to startTime");
+							deadline = startTime;
+						}
+						
+						executeCommand("update " + index +" -deadline " + deadline.replace("-", " "));
+					}else{
+						// Timed task
+						executeCommand("update " + index +" -begintime " + startTime.replace("-", " ") + " -endtime " + endTime.replace("-", " "));
+					}
+				}catch(Exception ex){
+					ex.printStackTrace();
+					log.log(Level.WARNING, "Exception encountered when editing task in table", ex);
+				}
+			}
+			if (column == MyTableModel.COL_END) {
+				log.info("End date changed");
+				
+				String endTime = (String) model.getValueAt(row, column);
+				String startTime = (String) model.getValueAt(row, MyTableModel.COL_START);
+				int index = row + 1;
+				
+				try{
+					if(startTime.isEmpty() && endTime.isEmpty()){
+						// Floating task
+						executeCommand("update " + index +" -tofloating");
+					}else if(startTime.isEmpty() || endTime.isEmpty()){
+						// Deadline task
+						String deadline;
+						if(startTime.isEmpty()){
+							deadline = endTime;
+						}else{
+							deadline = startTime;
+						}
+						
+						executeCommand("update " + index +" -deadline " + deadline.replace("-", " "));
+					} else {
+						// Timed task
+						executeCommand("update " + index + " -begintime " + startTime.replace("-", " ") + " -endtime " + endTime.replace("-", " "));
+					}
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+			if (column == MyTableModel.COL_TASKNAME) {
+				String newTaskName = (String) model.getValueAt(row, column);
+				int index = row + 1;
+				executeCommand("update " + index +" -name " + newTaskName);
+			}
+		}
+	}
+
+	class MyTableModel extends AbstractTableModel {
+		public static final long serialVersionUID = 8328597110205703514L;
+		public static final int COL_INDEX = 0;
+		public static final int COL_DONE = 1;
+		public static final int COL_START = 2;
+		public static final int COL_END = 3;
+		public static final int COL_TASKNAME = 4;
+
+		private String[] columnNames;
+		private List<Task> data;
+
+		public MyTableModel(List<Task> taskList){
+			log.entering(this.getClass().getName(), "<init>");
+			columnNames = new String[]{"Idx", "", "Start/Deadline", "End", "Task"};
+			data = taskList;
+			log.exiting(this.getClass().getName(), "<init>");
+		}
+
+		public int getColumnCount() {
+			log.fine("column count is " + columnNames.length);
+			return columnNames.length;
+		}
+
+		public int getRowCount() {
+			//return data.length;
+			log.fine("row count is " + data.size());
+			return data.size();
+		}
+
+		public String getColumnName(int col) {
+			log.fine(String.format("name of column %1d is %2s", col, columnNames[col]));
+			return columnNames[col];
+		}
+
+		public Object getValueAt(int row, int col) {
+			Task task = data.get(row);
+
+			String start, end;
+			if(task.getType().equals(TaskType.TIMED)) {
+				start = dateTimeToLongerString(task.getStartDate());
+			} else if(task.getType().equals(TaskType.DEADLINE)) {
+				start = dateTimeToLongerString(task.getDeadline());
+			} else {
+				start = TABLE_EMPTY_DATE_FIELD;
+			}
+
+			if(task.getType().equals(TaskType.TIMED)) {
+				end = dateTimeToLongerString(task.getEndDate());
+			} else {
+				end = TABLE_EMPTY_DATE_FIELD;
+			}
+
+			switch(col){
+			case COL_INDEX:
+				int index = row+1;
+				log.finest(String.format("value at row %1d col %2d is %3d", row, col, index));
+	    		return index;
+			case COL_DONE:
+				boolean isTaskDone = task.isDone();
+				log.finest(String.format("value at row %1d col %2d is %3b", row, col, isTaskDone));
+				return isTaskDone;
+			case COL_START:
+				log.finest(String.format("value at row %1d col %2d is %3s", row, col, start));
+				return start;
+			case COL_END:
+				log.finest(String.format("value at row %1d col %2d is %3s", row, col, end));
+				return end;
+			case COL_TASKNAME:
+				String taskName = task.getTaskName();
+				log.finest(String.format("value at row %1d col %2d is %3s", row, col, taskName));
+				return taskName;
+			default:
+				log.warning("Invalid column number");
+				assert false;
+			}
+			return null;
+		}
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		public Class getColumnClass(int c) {
+			log.fine(String.format("class of column %1d is %2s", c, getValueAt(0, c).getClass()));
+			return getValueAt(0, c).getClass();
+		}
+
+		public boolean isCellEditable(int row, int col) {
+			//Note that the data/cell address is constant,
+			//no matter where the cell appears on-screen.
+			if (col == COL_INDEX) {
+				log.fine(String.format("row %1d col %2d is editable: %3b", row, col, false));
+				return false;
+			} else {
+				log.fine(String.format("row %1d col %2d is editable: %3b", row, col, true));
+				return true;
+			}
+		}
+
+		/*
+		 * Don't need to implement this method unless your table's
+		 * data can change.
+		 */
+		public void setValueAt(Object value, int row, int col) {
+			log.entering(this.getClass().getName(), "setValueAt");
+			log.info(String.format("Setting row %1d col %2d to %3s", row, col, value));
+			
+			Task task = data.get(row);
+			
+
+			List<DateGroup> groups = null;
+			DateTime date;
+			
+			try{
+
+				switch (col) {
+				case COL_INDEX:
+					log.warning("Try to set value of index!");
+					assert false;
+					break;
+				case COL_DONE:
+					log.info("Setting value of done to " + value);
+					task.done((boolean) value);
+					break;
+				case COL_START:
+					log.fine("Setting value of start date with raw value " + value);
+					groups = nattyParser.parseWDefBaseDate(((String) value).replace("-", " "));
+					date = null;
+	
+					if (groups != null && !groups.isEmpty()) {
+						date = new DateTime(groups.get(0).getDates().get(0));
+					}
+	
+					if (date != null) {
+						log.fine("Managed to parse a date");
+						// Managed to parse out a date
+						if (task.getType() == TaskType.TIMED) {
+							log.info(String.format("Task is timed task, changing times to %1s %2s", date, task.getEndDate()));
+							task.changeStartAndEndDate(date, task.getEndDate());
+						} else if (task.getType() == TaskType.DEADLINE) {
+							log.info(String.format("Task is deadline task, changing times %1s", date));
+							task.changeDeadline(date);
+						} else if (task.getType() == TaskType.FLOATING) {
+							log.info(String.format("Task is floating task, changing to deadline tas with time %1s", date));
+							task.changetoDeadline(date);
+						}
+	
+					} else {
+						log.fine("Did not manage to parse a date");
+						// Did not manage to parse out a date
+						if (task.getType() == TaskType.TIMED) {
+							log.info(String.format("Task is timed task, changing to deadline task with time %1s", task.getEndDate()));
+							task.changetoDeadline(task.getEndDate());
+						} else if (task.getType() == TaskType.DEADLINE) {
+							log.info("Task is deadline task, changing to floating task");
+							task.changetoFloating();
+						}
+					}
+					break;
+				case COL_END:
+					log.fine("Setting value of end date with raw value " + value);
+					groups = nattyParser.parseWDefBaseDate(((String) value).replace("-", " "));
+					date = null;
+	
+					if (groups != null && !groups.isEmpty()) {
+						date = new DateTime(groups.get(0).getDates().get(0));
+					}
+	
+					if (date != null) {
+						log.fine("Managed to parse a date");
+						
+						// Managed to parse out a date
+						if (task.getType() == TaskType.TIMED) {
+							log.info(String.format("Task is timed task, changing times to %1s %2s", task.getStartDate(), date));
+							task.changeStartAndEndDate(task.getStartDate(), date);
+						} else if (task.getType() == TaskType.DEADLINE) {
+							log.info(String.format("Task is deadline task, changing to timed task %1s %2s", task.getDeadline(), date));
+							task.changetoTimed(task.getDeadline(), date);
+						} else if (task.getType() == TaskType.FLOATING) {
+							log.info(String.format("Task is floating task, changing to deadline tas with time %1s", date));
+							task.changetoDeadline(date);
+						}
+					} else {					
+						log.fine("Did not manage to parse a date");
+						
+						// Did not manage to parse out a date
+						if (task.getType() == TaskType.TIMED) {
+							log.info(String.format("Task is timed task, changing to deadline task with time %1s", task.getStartDate()));
+							task.changetoDeadline(task.getStartDate());
+						}
+					}
+	
+					break;
+				case COL_TASKNAME:
+					log.info("Changing task name to " + value);
+					task.changeName((String) value);
+					break;
+				default:
+					assert false;
+				}
+			
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+
+			fireTableCellUpdated(row, col);
+			
+			log.exiting(this.getClass().getName(), "setValueAt");
+		}
+	}
+	
+	public int getContentWidth(String content) {
+		log.entering(this.getClass().getName(), "getContentWidth");
+		
+	    JLabel dummylabel = new JLabel();
+	    //dummyEditorPane.setSize(1, Short.MAX_VALUE);
+	    dummylabel.setText(content);
+	    int width = dummylabel.getPreferredSize().width;
+	    
+	    log.finest(String.format("Width of string %1s is %2d", content, width));
+	    
+	    log.exiting(this.getClass().getName(), "getContentWidth");
+		return width;
+	}
+
+	public void showTasksListInTable(List<Task> taskListOrig){
+		List<Task> taskList = new ArrayList<Task>();
+		for(Task t : taskListOrig){
+			Task newTask = new Task(t);
+			taskList.add(newTask);
+		}
+		
+		log.entering(this.getClass().getName(), "showTasksList");
+		
+		int indexNumberColumnWidth = getContentWidth(TABLE_COLUMN_WIDTH_INDEX_MAX_TEXT) + TABLE_COLUMN_WIDTH_EXTRA;
+		int checkboxColumnWidth = TABLE_COLUMN_WIDTH_CHECKBOX;
+		int dateColumnWidth = getContentWidth(TABLE_COLUMN_WIDTH_DATE_MAX_TEXT) + TABLE_COLUMN_WIDTH_EXTRA;
+
+		table.setModel(new MyTableModel(taskList));
+		table.getColumnModel().getColumn(0).setMinWidth(indexNumberColumnWidth);
+		table.getColumnModel().getColumn(0).setMaxWidth(indexNumberColumnWidth);
+		table.getColumnModel().getColumn(1).setMinWidth(checkboxColumnWidth);
+		table.getColumnModel().getColumn(1).setMaxWidth(checkboxColumnWidth);
+		// Give more space to date: Test with 27 May 2009 10am to 27 May 2009 10pm
+		table.getColumnModel().getColumn(2).setMinWidth(dateColumnWidth);
+		table.getColumnModel().getColumn(2).setMaxWidth(dateColumnWidth);
+		table.getColumnModel().getColumn(3).setMinWidth(dateColumnWidth);
+		table.getColumnModel().getColumn(3).setMaxWidth(dateColumnWidth);
+		//table.getColumnModel().getColumn(4).setPreferredWidth(160);
+		
+		table.getModel().addTableModelListener(new TableChangedHandler());
+		
+		log.exiting(this.getClass().getName(), "showTasksList");
 	}
 
 }
