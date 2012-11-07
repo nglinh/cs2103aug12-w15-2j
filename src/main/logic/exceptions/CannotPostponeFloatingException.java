5d007e087c43e4e5ae92c9c677f48ced4187c88b
Hien Linh Nguyen <mrlinhnguyenhien@gmail.com>
2012-11-07 17:19:21 +0800

    Fixed bug in delete over

---
+++ src/main/logic/exceptions/CannotPostponeFloatingException.java
@@ -0,0 +1,8 @@
+package main.logic.exceptions;
+
+public class CannotPostponeFloatingException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public CannotPostponeFloatingException (){
+		super();
+	}
+}
---
+++ src/main/logic/exceptions/NoSuchCommandException.java
@@ -0,0 +1,8 @@
+package main.logic.exceptions;
+
+public class NoSuchCommandException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public NoSuchCommandException (){
+		super();
+	}
+}
