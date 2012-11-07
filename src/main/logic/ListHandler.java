bdc58ff716a4924f129cc383828deaca3a59082e
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm>
2012-11-07 21:37:34 +0800

    Sort + viewing handlers: Updated the variable nam from latestRefreshHandler to latestListingHandler

---
+++ src/main/logic/ListHandler.java
@@ -0,0 +1,85 @@
+package main.logic;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import main.shared.LogicToUi;
+import main.shared.SearchTerms;
+import main.shared.Task;
+
+public class ListHandler extends CommandHandler {
+
+	private ListParser parser;
+
+	public ListHandler(String arguments) {
+		super(arguments);
+		parser = new ListParser(arguments);
+	}
+
+	@Override
+	public LogicToUi execute() {
+
+		parser.parse();
+		List<Task> results;
+
+		SearchTerms filter;
+
+		if (parser.overdue) {
+			DateTime startDate = new DateTime(Long.MIN_VALUE);
+			DateTime endDate = new DateTime();
+
+			filter = new SearchTerms(false, true, false, false, false,
+					startDate, endDate);
+
+			results = dataBase.search(filter);
+
+			String overdueStatusMsg = "Listing based on these parameters: \"overdue\" ";
+
+			lastShownObject.setLastShownList(results);
+			latestListingHandlerForUI = this;
+			return new LogicToUi(results, overdueStatusMsg, filter);
+		}
+
+
+
+		if (parser.today && parser.tomorrow) {
+			DateTime startDate = new DateTime().withTimeAtStartOfDay();
+			DateTime endDate = startDate.plusDays(1).withTime(23, 59, 59, 999);
+
+			filter = new SearchTerms(parser.complete, parser.incomplete, parser.timed, parser.deadline,
+					parser.floating, startDate, endDate);
+		} else if (parser.today) {
+
+			DateTime startDate = new DateTime().withTimeAtStartOfDay();
+			DateTime endDate = startDate.withTime(23, 59, 59, 999);
+
+			filter = new SearchTerms(parser.complete, parser.incomplete, parser.timed, parser.deadline,
+					parser.floating, startDate, endDate);
+
+
+		} else if (parser.tomorrow) {
+			DateTime startDate = new DateTime().plusDays(1)
+					.withTimeAtStartOfDay();
+			DateTime endDate = startDate.plusDays(1).withTime(23, 59, 59, 999);
+
+			filter = new SearchTerms(parser.complete,parser.incomplete, parser.timed, parser.deadline,
+					parser.floating, startDate, endDate);
+
+		} else {
+			filter = new SearchTerms(parser.complete, parser.incomplete, parser.timed, parser.deadline,
+					parser.floating);
+		}
+
+		results = dataBase.search(filter);
+
+		lastShownObject.setLastShownList(results);
+		latestListingHandlerForUI = this;
+		return new LogicToUi(results, parser.statusMsg, filter);
+
+
+	}
+
+
+
+}
