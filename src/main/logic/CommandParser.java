9c5f3d6b15e99d546cc17a0302a806d79ca273b6
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm>
2012-11-08 00:37:53 +0800

    FileMgmt: Will now prepopulate the database task list if no database file present. Assume first launch

---
+++ src/main/logic/CommandParser.java
@@ -0,0 +1,32 @@
+package main.logic;
+
+import main.logic.exceptions.CannotParseDateException;
+import main.logic.exceptions.CannotPostponeFloatingException;
+import main.logic.exceptions.EmptyDescriptionException;
+
+public abstract class CommandParser {
+	protected static final int INT_2 = 2;
+	protected static final int INT_1 = 1;
+	protected static final int INT_0 = 0;
+	protected static LastShownToUI lastShownObject = LastShownToUI
+			.getInstance();
+
+	public CommandParser(String arguments) {
+	}
+
+	public abstract void parse() throws EmptyDescriptionException,
+			CannotParseDateException, CannotPostponeFloatingException;
+
+	protected String removeFirstWord(String string) {
+		return string.replaceFirst(getFirstWord(string), "").trim();
+	}
+
+	protected String getFirstWord(String string) {
+		return string.split(" ")[0];
+	}
+
+	protected String[] splitArgumentsBySpaces(String arguments) {
+		return arguments.split(" ");
+	}
+
+}
---
+++ src/main/logic/ListParser.java
@@ -0,0 +1,116 @@
+//@author A0081007U
+package main.logic;
+
+
+
+public class ListParser extends CommandParser {
+
+	private String arguments;
+	private String statusMsg;
+	private boolean overdue = false;
+	private boolean complete = false;
+	private boolean incomplete = false;
+	private boolean timed = false;
+	private boolean deadline = false;
+	private boolean floating = false;
+	private boolean today = false;
+	private boolean tomorrow = false;
+
+	public ListParser(String arguments) {
+		super(arguments);
+		this.arguments = arguments;
+	}
+
+	@Override
+	public void parse(){
+		
+		statusMsg = "Listing based on these parameters: ";
+
+		String[] parameters = splitArgumentsBySpaces(arguments);
+
+		for (String eachParam : parameters) {
+
+			if (eachParam.equals("overdue")) {
+				overdue = true;
+				statusMsg += " \"overdue\" ";
+			}
+			
+			if (eachParam.equals("completed") || eachParam.equals("done")) {
+				complete = true;
+				statusMsg += " \"complete\" ";
+			}
+
+			if (eachParam.equals("incomplete") || eachParam.equals("undone")) {
+				incomplete = true;
+				statusMsg += " \"undone\" ";
+			}
+
+			if (eachParam.equals("timed")) {
+				timed = true;
+				statusMsg += " \"timed\" ";
+			}
+
+			if (eachParam.equals("deadline")) {
+				deadline = true;
+				statusMsg += " \"deadline\" ";
+			}
+
+			if (eachParam.equals("floating")) {
+				floating = true;
+				statusMsg += " \"floating\" ";
+			}
+
+			if (eachParam.equals("today")) {
+				today = true;
+				statusMsg += " \"today\" ";
+			}
+
+			if (eachParam.equals("tomorrow")) {
+				tomorrow = true;
+				statusMsg += " \"tomorrow\" ";
+			}
+
+		}
+	}
+	
+	public String getStatusMsg(){
+		return statusMsg;
+	}
+	
+	public boolean isOverdue(){
+		return overdue;
+	}
+	
+	public boolean isComplete(){
+		return complete;
+	}
+	
+	public boolean isIncomplete(){
+		return incomplete;
+	}
+	
+	public boolean isTimed(){
+		return timed;
+	}
+	
+	public boolean isDeadline(){
+		return deadline;
+	}
+	
+	public boolean isFloating(){
+		return floating;
+	}
+	
+	public boolean isToday(){
+		return today;
+	}
+	
+	public boolean isTomorrow(){
+		return tomorrow;
+	}
+	
+	
+
+
+
+}
