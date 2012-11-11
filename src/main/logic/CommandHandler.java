0a499bcf4a4dcbd59e71f03bee1461bd086aa3cc
mrlinh <mrlinh@mrlinh-HP>
2012-11-11 14:40:08 +0800

    Add some docs for CommandHandler class

--- src/main/logic/CommandHandler.java
+++ src/main/logic/CommandHandler.java
@@ -13,7 +13,16 @@ import main.shared.LogicToUi;
 import main.shared.Task;
 import main.storage.Database;
 import main.storage.WillNotWriteToCorruptFileException;
-
+/**
+ * This class is to be extended by any supported command. It provide
+ * back-bone APIs needed for any handlers to execute the command.
+ * 
+ * The intention of this class is to allow easy extension for new features
+ * without touching the current supported ones.
+ * 
+ * @author A0088427U
+ *
+ */
 public abstract class CommandHandler {
 
 	protected static final String ERROR_CANNOT_PARSE_DATE = "One or more field(s)"
@@ -39,15 +48,21 @@ public abstract class CommandHandler {
 	protected static CommandHandler latestSortHandlerForUI = new SortHandler("");
 
 
-	protected LogicToUi feedback;
+	protected LogicToUi feedback;	//All handlers will return a LogicToUi object.
 
-
+	/**
+	 * Empty constructor, to be overriden by handlers' constructor
+	 * @param arguments: argument of the command.
+	 */
 	public CommandHandler(String arguments) {
 	}
 
-
+	/**
+	 * To be implemented by all handlers.
+	 * @return LogicToUi object to be displayed on UI
+	 */
 	public abstract LogicToUi execute();
-
+	
 	protected void pushUndoStatMesNTaskList(String undoMsg,
 			List<Task> currentCopy) {
 		undoMsgHistory.push(undoMsg);
