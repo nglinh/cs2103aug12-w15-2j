ac8fbd838ab9c5f7e9b0f27102448c8a44ec94db
mrlinh <mrlinh@mrlinh-HP>
2012-11-11 20:52:45 +0800

    Added new files for testing Done, undone postpone and search. NOTE: note yet implemented.

---
+++ src/test/logic/LogicSearchTest.java
@@ -0,0 +1,14 @@
+package test.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LogicSearchTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
