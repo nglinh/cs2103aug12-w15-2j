85771b40564bd44ce703ed8c1277c5001bfd9e0f
mrlinh <mrlinh@mrlinh-HP>
2012-11-11 20:52:45 +0800

    Added new files for testing Done, undone postpone and search. NOTE: note yet implemented.

---
+++ src/test/logic/LogicDoneAndUndoneTest.java
@@ -0,0 +1,14 @@
+package test.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LogicDoneAndUndoneTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
