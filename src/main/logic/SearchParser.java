fd34a7fbbe15dc6fd24b3c21d00daf3bcd833b59
Hien Linh Nguyen <mrlinhnguyenhien@gmail.com>
2012-11-12 11:08:46 +0800

    Added documentation for search parser class

--- src/main/logic/SearchParser.java
+++ src/main/logic/SearchParser.java
@@ -2,29 +2,42 @@
 
 package main.logic;
 
-public class SearchParser extends CommandParser{
+/**
+ * An object of this class parse a search command. Each object of searchparser
+ * associates with only one search handler object.
+ * 
+ */
+public class SearchParser extends CommandParser {
 
 	private String arguments;
 	private String[] keywords = new String[0];
-	
-	
+
+	/**
+	 * Constructor of the class
+	 * 
+	 * @param arguments
+	 *            : argument string associating with the command
+	 */
 	public SearchParser(String arguments) {
 		super(arguments);
 		this.arguments = arguments;
 	}
 
+	/**
+	 * This method override the parse method in command parser class.
+	 */
 	@Override
-	public void parse(){
-		
+	public void parse() {
+
 		if (arguments.length() == 0) {
 			return;
 		}
-		
+
 		keywords = arguments.split(" ");
 
 	}
-	
-	public String[] getKeyWords(){
+
+	public String[] getKeyWords() {
 		return keywords;
 	}
 
