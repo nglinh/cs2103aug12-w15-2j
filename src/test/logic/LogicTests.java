a4af811aba592c717fc5598bbe24c243996742de
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm.comp.nus.edu.sg>
2012-11-12 10:31:41 +0800

    LogicTests: Added Postponetest to suite

--- src/test/logic/LogicTests.java
+++ src/test/logic/LogicTests.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AddTest.class, DeleteTest.class, EditTest.class, ListTest.class, SearchTest.class,
-		DoneTest.class, SortTest.class, UndoneTest.class, UndoTest.class })
+		DoneTest.class, SortTest.class, UndoneTest.class, UndoTest.class, PostponeTest.class})
 public class LogicTests {
 
 }
