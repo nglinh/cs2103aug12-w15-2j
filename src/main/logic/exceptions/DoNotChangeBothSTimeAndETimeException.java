126beb6a9cfb0ab5ebe72362d2b7acc4f80eff8b
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm.comp.nus.edu.sg>
2012-11-12 10:54:51 +0800

    Tests: Added more out of boundary tests

---
+++ src/main/logic/exceptions/DoNotChangeBothSTimeAndETimeException.java
@@ -0,0 +1,11 @@
+package main.logic.exceptions;
+
+public class DoNotChangeBothSTimeAndETimeException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public DoNotChangeBothSTimeAndETimeException() {
+		super();
+	}
+
+}
