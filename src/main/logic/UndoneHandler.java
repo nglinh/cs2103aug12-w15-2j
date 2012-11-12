bd0fec8a8b341237b35071972d2f93fb8fbdf676
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm.comp.nus.edu.sg>
2012-11-12 10:30:21 +0800

    PostponeTest: Renamed

---
+++ src/main/logic/UndoneHandler.java
@@ -0,0 +1,71 @@
+//@author A0081007U
+package main.logic;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import main.shared.LogicToUi;
+import main.shared.Task;
+import main.storage.WillNotWriteToCorruptFileException;
+
+public class UndoneHandler extends CommandHandler {
+
+	private String arguments;
+	private UndoneParser parser;
+	private Task copy;
+	private Task toBeUpdated;
+
+	public UndoneHandler(String arguments) {
+		super(arguments);
+		this.arguments = arguments;
+		parser = new UndoneParser(arguments);
+	}
+
+	@Override
+	public LogicToUi execute() {
+
+		if (arguments.length() == 0) {
+			return new LogicToUi(ERROR_INDEX_NUMBER_NOT_VALID);
+		}
+
+		try {
+			parser.parse();
+			int serial = parser.getSerialOfTask();
+
+			toBeUpdated = dataBase.locateATask(serial);
+			copy = new Task(toBeUpdated);
+			copy.done(false);
+			if (copy.isEqualTo(toBeUpdated)) {
+				feedback = new LogicToUi(taskToString(toBeUpdated)
+						+ " has been already been marked as undone.");
+			} else {
+				updateDatabaseNSendToUndoStack();
+				feedback = new LogicToUi(taskToString(toBeUpdated)
+						+ " has been marked as undone.", serial);
+			}
+
+		} catch (NumberFormatException e) {
+			feedback = new LogicToUi(ERROR_INDEX_NUMBER_NOT_VALID);
+		} catch (NoSuchElementException e) {
+			return new LogicToUi(ERROR_INDEX_NUMBER_NOT_VALID);
+		} catch (IOException e) {
+			feedback = new LogicToUi(ERROR_IO);
+		} catch (WillNotWriteToCorruptFileException e) {
+			feedback = new LogicToUi(ERROR_FILE_CORRUPTED);
+		}
+		return feedback;
+	}
+
+	@Override
+	protected void updateDatabaseNSendToUndoStack()
+			throws NoSuchElementException, IOException,
+			WillNotWriteToCorruptFileException {
+		List<Task> copyCurrentTaskList = super.getCurrentTaskList();
+		dataBase.update(toBeUpdated.getSerial(), copy);
+		String taskDetails = taskToString(copy);
+		String undoMessage = "marking of task \"" + taskDetails
+				+ "\" as undone";
+		super.pushUndoStatMesNTaskList(undoMessage, copyCurrentTaskList);
+	}
+
+}
---
+++ src/test/logic/PostponeTest.java
@@ -0,0 +1,14 @@
+package test.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PostponeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
