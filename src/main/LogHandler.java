e10265e99e57a091cfc705f7a6d22d6953dada91
Yeo Kheng Meng <Yeo Kheng Meng@t410-w7p64-ykm.comp.nus.edu.sg>
2012-11-12 19:39:36 +0800

    LogHandler: Removed unused import

--- src/main/LogHandler.java
+++ src/main/LogHandler.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SocketHandler;
 
 public class LogHandler {
 
